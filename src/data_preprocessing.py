0dae558da194f26363d6c1e64264d9c9f405c8b1
JIANG-Rx <54588043+JIANG-Rx@users.noreply.github.com>
2020-11-13 00:11:16 +0800

    codes cleanup

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,17 +1,22 @@
 # %%
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
+from imblearn import over_sampling
 from sklearn.ensemble import ExtraTreesClassifier
+import sklearn.decomposition
 
-# %%
+
+# %% load data
 train = pd.read_csv('../data/train.csv')
 test = pd.read_csv('../data/test.csv')
 
 labels = train.columns.drop(['id', 'target'])
 target = train['target']
 
-# %%
+# %% feature selection based on DT classifier
+
 TOP_FEATURES = 15
 
 forest = ExtraTreesClassifier(n_estimators=250, max_depth=5, random_state=1)
@@ -25,13 +30,8 @@ std = np.std(
 indices = np.argsort(importances)[::-1]
 indices = indices[:TOP_FEATURES]
 
-print('Top features:')
-for f in range(TOP_FEATURES):
-    print('%d. feature %d (%f)' % (f + 1, indices[f], importances[indices[f]]))
-
-# %%
 plt.figure()
-plt.title('Top feature importances')
+plt.title('Top feature importances [2]')
 plt.bar(
     range(TOP_FEATURES),
     importances[indices],
@@ -39,5 +39,23 @@ plt.bar(
 )
 plt.xticks(range(TOP_FEATURES), indices)
 plt.show()
+
+# %% Boardline SMOTE
+sm = over_sampling.BorderlineSMOTE(
+    random_state=42, kind="borderline-1")
+
+X_train = train.drop(['id', 'target'], axis=1)
+y_train = train['target']
+X_res, y_res = sm.fit_resample(X_train, y_train)
+
+
+trainDF = pd.DataFrame(X_res)
+dim_reduced = TruncatedSVD(n_components=2, random_state=34)
+dim_reduced_result = pd.DataFrame(dim_reduced.fit_transform(X_res))
+
+
+plt.title("Train data after Boarderline SMOTE")
+
+sns.scatterplot(x=dim_reduced_result[0], y=dim_reduced_result[1], hue=y_res)
 
 # %%
--- src/data_visualization.py
+++ src/data_visualization.py
@@ -22,13 +22,13 @@ train = pd.read_csv("..\\data\\train.csv")
 
 # %% train data distribution
 trainDF = pd.DataFrame(train)
-pca = sklearn.decomposition.TruncatedSVD(n_components=2, random_state=14)
-pca_result = pd.DataFrame(pca.fit_transform(trainDF.iloc[:, 2:302]))
+svd = sklearn.decomposition.TruncatedSVD(n_components=2, random_state=14)
+svd_result = pd.DataFrame(svd.fit_transform(trainDF.iloc[:, 2:302]))
 
 
-plt.title("Train data under SVD decomposition")
+plt.title("Train data under SVD decomposition[1]")
 
-sns.scatterplot(x=pca_result[0], y=pca_result[1], hue=trainDF["target"])
+sns.scatterplot(x=svd_result[0], y=svd_result[1], hue=trainDF["target"])
 
 # %% class imbalance
 train['target'].value_counts().plot(kind='bar', title='Count (target)')
@@ -44,6 +44,7 @@ plt.legend()
 plt.show()
 
 # %% feature distribution
+# derived from https://www.kaggle.com/gpreda/overfitting-the-private-leaderboard
 
 
 def plot_feature_distribution(df1, df2, label1, label2, features):
@@ -54,7 +55,7 @@ def plot_feature_distribution(df1, df2, label1, label2, features):
 
     for feature in features:
         i += 1
-        plt.subplot(10, 10, i)
+        plt.subplot(5, 5, i)
         sns.kdeplot(df1[feature], bw=0.5, label=label1)
         sns.kdeplot(df2[feature], bw=0.5, label=label2)
         plt.xlabel(feature, fontsize=9)
@@ -69,37 +70,12 @@ t1 = train.loc[train['target'] == 1]
 features = train.columns.values[2:102]
 plot_feature_distribution(t0, t1, '0', '1', features)
 
-# %% attribute value
-test = pd.read_csv('../data/test.csv')
 
-labels = train.columns.drop(['id', 'target'])
-target = train['target']
-TOP_FEATURES = 15
-
-forest = ExtraTreesClassifier(n_estimators=250, max_depth=5, random_state=1)
-forest.fit(train[labels], train['target'])
-
-importances = forest.feature_importances_
-std = np.std(
-    [tree.feature_importances_ for tree in forest.estimators_],
-    axis=0
-)
-indices = np.argsort(importances)[::-1]
-indices = indices[:TOP_FEATURES]
-
-plt.figure()
-plt.title('Top feature importances')
-plt.bar(
-    range(TOP_FEATURES),
-    importances[indices],
-    yerr=std[indices],
-)
-plt.xticks(range(TOP_FEATURES), indices)
-plt.show()
 
 # %% Data correlation
 correlations = train[features].corr()
 for i in range(correlations.shape[0]):
+    # not correlated with itself
     correlations['{}'.format(i)]['{}'.format(i)] = 0
 plt.title("Correlations between attributes")
 sns.distplot(correlations, hist=True, rug=True)
